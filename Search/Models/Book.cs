f1eed3e4317269eef32e50c378824ca68d11a4b2
wasimkhawja <wasimkhawja@gmail.com>
2015-11-22 20:03:55 -0600

    Search
    Adds Books and has Search Filter

---
+++ Search/Models/Book.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace Search.Models
+{
+    public class Book
+    {
+         [Key]
+        public int SKU { get; set; }
+        public string Title { get; set; }
+        public string AuthorFirst { get; set; }
+        public string AuthorLast { get; set; }
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+    public class BookDBContext : DbContext
+
+    {
+        public DbSet<Book> Books { get; set; }
+        
+    }
+}
